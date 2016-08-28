37f811a575fb16afe65147ff7831fed07ae52c9d
brian buckley <briankbuckley@gmail.com>
2016-08-28 12:54:17 -0400

    tweaks

--- script.jsx
+++ script.jsx
@@ -21,14 +21,33 @@
 //   return i2 - i1;
 // }
 
-var xxx = 888;
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 var Hello = React.createClass({
+
+  ages: function (d1, d2) {
+    return [new Date(d1), new Date(d2)];
+  },
+
+  // age: function (d1, d2) {
+  //   return d2.getFullYear() - d1.getFullYear();
+  // },
+
+  age_hir: function () {
+    // va d1 = new Date(this.props.dob);
+    // va d2 = new Date(this.props.doe);
+  return (new Date(this.props.doe)).getFullYear() - (new Date(this.props.dob)).getFullYear();
+  },
 
   render: function(){
     return (
       <div>
-        {this.props.name}.  I was born on {this.props.dob} ({xxx} years old).
+       {this.props.name}.  I was born on {this.props.dob} ({this.age_hir()} years old).
+        <br/>
+       {this.props.name}.  I was born on {this.props.dob}.
       </div>
     )
   }
@@ -52,11 +71,65 @@ var ThreeHelloDob = React.createClass({
   render: function(){
     return (
       <div>
-        <Hello name="Karl" dob='12/2/1960'/>
-        <Hello name="David" dob='11/12/1957'/>
-        <Hello name="Jim" dob='6/30/1964'/>
+        <Hello name="Karl"    dob='12/2/1960'  doe='1/1/1984' crd='1/2/2020'/>
+        <Hello name="David"   dob='11/12/1957' doe='6/16/1982' crd=''/>
+        <Hello name="Ralph"   dob='10/10/1962' crd=''/>
+        <Hello name="Jim"     dob='6/30/1964'  doe='7/1/1987' crd=''/>
+        <Hello name="Unknown" crd=''/>
+        <Hello name="Brian"   dob='12/11/1958' doe='6/30/1992' crd=''/>
       </div>
     )
   }
 });
-React.render(<ThreeHelloDob/>, document.getElementById("root"));
+// React.render(<ThreeHelloDob/>, document.getElementById("root"));
+
+var ThreeListHello = React.createClass({
+
+
+  render: function(){
+    return (
+      <div>
+        <Hello name="Karl"    dob='12/2/1960'  doe='1/1/1984' crd='1/2/2020'/>
+        <Hello name="David"   dob='11/12/1957' doe='6/16/1982' crd=''/>
+        <Hello name="Ralph"   dob='10/10/1962' crd=''/>
+        <Hello name="Jim"     dob='6/30/1964'  doe='7/1/1987' crd=''/>
+        <Hello name="Unknown" crd=''/>
+        <Hello name="Brian"   dob='12/11/1958' doe='6/30/1992' crd=''/>
+      </div>
+    )
+  }
+});
+//ReactDOM.render(<ThreeListHello/>, document.getElementById("root"));
+
+// var HelloState = React.createClass({
+export default class HelloState extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      name: "Buckley",
+      dob: '12/11/1958',
+      doe: '6/30/1992'
+    }
+  }
+
+  dob(){
+    return new Date(this.state.dob);
+  }
+  doe(){
+    return new Date(this.state.doe);
+  }
+
+  age(){
+    return this.doe().getFullYear() - this.dob().getFullYear() ;
+  }
+
+  render(){
+    return (
+      <div>
+       {this.state.name}.  I was born on {this.state.dob} and hired on {this.state.doe}.  Age is {this.age()}.
+      </div>
+    )
+  }
+
+}
+ReactDOM.render(<HelloState crd='1/1/2020'/>, document.getElementById("root"));
